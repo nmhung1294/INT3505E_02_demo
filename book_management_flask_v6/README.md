9a0a4078f24c8c069dcfb942217598a0f8c8fadc
nmhung1294 <144262235+nmhung1294@users.noreply.github.com>
2025-10-16 09:00:31 +0700

    init V6

---
+++ book_management_flask_v6/README.md
@@ -0,0 +1,20 @@
+-# Book Management Flask (v6)
+Data modeling & Resource Design
+
+Từ thư mục gốc của repository:
+1) Cài đặt dependencies:
+
+```powershell
+pip install Flask Flask-SQLAlchemy flasgger flask-swagger-ui PyJWT
+```
+
+2) Chạy ứng dụng:
+
+```powershell
+python -m book_management_flask_v6.run
+```
+
+3) Mở trình duyệt:
+- Swagger UI: http://127.0.0.1:5000/swagger hoặc http://127.0.0.1:5000/apidocs
+- OpenAPI YAML (raw): http://127.0.0.1:5000/openapi.yaml
+
---
+++ book_management_flask_v6/__init__.py
@@ -0,0 +1,40 @@
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flasgger import Swagger  # Thêm import này
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///library.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'supersecretkey'
+
+db = SQLAlchemy(app)
+swagger = Swagger(app, template={
+    "swagger": "2.0",
+    "info": {
+        "title": "Library API",
+        "description": "Simple API for library management (JWT Authentication supported)",
+        "version": "1.0"
+    },
+    "basePath": "/api", 
+    "securityDefinitions": {  
+        "Bearer": {
+            "type": "apiKey",
+            "name": "Authorization",
+            "in": "header",
+            "description": "JWT Authorization header using the Bearer scheme. Example: 'Bearer {token}'"
+        }
+    },
+    "security": [ 
+        {"Bearer": []}
+    ]
+})
+
+
+from .models import Book, User, Borrowing
+from .api import api as api_blueprint
+
+app.register_blueprint(api_blueprint, url_prefix='/api')
+
+with app.app_context():
+    db.create_all()
