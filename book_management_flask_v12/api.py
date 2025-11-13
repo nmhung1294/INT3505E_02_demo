e83a2b2b25ff67e14d8c7e618db34ebd0a24ab79
nmhung1294 <144262235+nmhung1294@users.noreply.github.com>
2025-11-13 16:39:25 +0700

    remove-v12

--- book_management_flask_v12/api.py
+++
@@ -1,8 +0,0 @@
-
-from flask import Blueprint
-
-api = Blueprint("api", __name__, url_prefix="/api")
-
-# ==========================================
-# APIs will be generated from OpenAPI spec
-# ==========================================
